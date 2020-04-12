cd20044fe7a9d12dd56f07b489bb4375509f25b6
Alexander Profeit <alex@profeit.com>
2020-04-12 18:37:43 -0400

    add stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+REDIRECT_ROOT = Raisls.env.development? ? "http:localhost:3001" :  "https://chat.profeit.com" 
 Rails.application.routes.draw do
   root to: redirect("https://chat.profeit.com")
 end
